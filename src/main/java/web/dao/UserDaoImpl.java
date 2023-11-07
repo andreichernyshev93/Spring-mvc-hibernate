055ddaf7f10b5850868e99bba12a63e9c45ac9a0
Andrew Chernyshev <andrei.chernyshev93@mail.ru>
2023-11-07 22:13:38 +0300

    Выполнено

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoImpl implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public User show(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void update(Long id, User updatedUser) {
+        entityManager.merge(updatedUser);
+    }
+
+    @Override
+    public void delete(Long id) {
+        entityManager.remove(show(id));
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,92 @@
+package web.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Column(name = "name")
+    private String firstName;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
+    private int age;
+
+    @Email(message = "Email should be valid")
+    @Column(name = "email")
+    private String email;
+
+    public User() {}
+
+    public User(String firstName, String lastName, int age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
